d604c1ab1429e732fa2870338eaeaa8df6dff29c
Christopher Whitney <chrisswhitneyy@gmail.com>
2020-12-22 10:05:46 -0700

    Intial commit

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/not the text/i);
+  expect(linkElement).toBeInTheDocument();
+});
